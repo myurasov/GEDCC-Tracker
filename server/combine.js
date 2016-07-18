522d5e4b0fe0a46126daf9afd33603e2f798ca48
Mikhail Yurasov <me@yurasov.me>
2016-07-18 13:28:13 -0700

    Save activities to file

--- server/combine.js
+++ server/combine.js
@@ -7,6 +7,7 @@ const fs = require('fs');
 const c = require('colors');
 
 function combine() {
+  const out = `${__dirname}/data/all-activities.json`;
   const clubActivities = {};
   const athletes = new Set();
   let dataFiles = 0;
@@ -16,18 +17,25 @@ function combine() {
     dataFiles++;
 
     try {
-      const activities = require(`./data/dumps/${f}`);
+      const activities = require(`${__dirname}/data/dumps/${f}`);
       for (const activity of activities) {
         clubActivities[activity.id] = activity;
         athletes.add(activity.athlete.id);
       }
     } catch (e) {
+      console.error(`Unable to read data file "${f}"`);;
     }
+
   }
 
+  // info
   console.log(c.cyan('Data files:\t\t') + dataFiles);
   console.log(c.cyan('Activities:\t\t') + _.values(clubActivities).length);
   console.log(c.cyan('Active athletes:\t') + athletes.size);
+
+  // save to file
+  fs.writeFileSync(out, JSON.stringify(clubActivities));
+  console.log(c.cyan('File created:\t\t') + out);
 }
 
 combine();
