84abed1c5e56753d5ccaa6b2e4d7852bdac7ad49
Mikhail Yurasov <me@yurasov.me>
2016-07-30 21:44:34 -0700

    Refresh every 1..2 minutes

--- web-app/src/web/app/screen.main.controller.js
+++ web-app/src/web/app/screen.main.controller.js
@@ -3,13 +3,21 @@
  * @author Mikhail Yurasov <me@yurasov.me>
  */
 
-export default /* @ngInject */ ($scope, DataRepository) => {
+export default /* @ngInject */ ($scope, DataRepository, $timeout) => {
+  /**
+   * Refresh data
+   */
+  function refresh() {
+    DataRepository.getTop()
+      .then(v => {
+        $scope.athletesTop = v.athletes;
+        $scope.updatedAt = DataRepository.getUpdatedAt();
+        $scope.$digest();
+      });
 
-  DataRepository.getTop()
-    .then(v => {
-      $scope.athletesTop = v.athletes;
-      $scope.updatedAt = DataRepository.getUpdatedAt();
-      $scope.$digest();
-    })
+    // repeat in 1..2 minutes
+    $timeout(refresh, 60000 + Math.random() * 60000);
+  }
 
+  refresh();
 }
