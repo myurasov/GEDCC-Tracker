b4d3e4e9899052ddb73abe4d2731f7353fda743e
Mikhail Yurasov <me@yurasov.me>
2016-08-16 18:25:37 -0700

    Backup data files

--- dev/scripts/refresh.sh
+++ dev/scripts/refresh.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-localRoot="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/../.."
+localRoot="$(cd "$( dirname "${BASH_SOURCE[0]}" )/../.." && pwd )"
 
 killall firefox
 
@@ -9,5 +9,10 @@ ${localRoot}/crawler/src/app.js get-activities -dD
 
 cp -f ${localRoot}/crawler/data/athletes.json ${localRoot}/web-app/src/web/resources/
 cp -f ${localRoot}/crawler/data/activities.json ${localRoot}/web-app/src/web/resources/
+
+# backup files
+backupDir="/var/log/gedcc-back/$(date +%F_%T)"
+mkdir -pv ${backupDir}
+cp -v ${localRoot}/web-app/src/web/resources/*.* ${backupDir}
 
 killall firefox
